7138de1d58c0214b722b44f6eae76df6b491e395
69011593-creator <69011593@kmitl.ac.th>
2026-07-17 08:47:53 +0000

    Submit lab 2

---
+++ week2/Quiz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("%-13s: %s\n" , "Programing" , "A");
+    printf("%-13s: %s\n" , "Drawing" , "B+");
+    printf("%-13s: %s\n" , "Mechanics" , "B+");
+    printf("%-13s: %s\n" , "Math" , "B");
+    printf("%-13s: %0.1f\n" , "GPs" , 3.5);
+    return 0;
+
+}
